2bd3a4076cd25a3c991c290a1303ba975cdd04ac
maxsitnikov <makc.kekc@gmail.com>
2022-04-28 20:07:31 +0300

    Refactor methods, add tf Metrics, add visualization

--- dogsml/tfbased/convolutional.py
+++ dogsml/tfbased/convolutional.py
@@ -1,32 +1,21 @@
 import h5py
+import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
 import tensorflow.keras.layers as tfl
 
 import dogsml.utils
 
 
-def neural_net_model(
-        x_train,
-        y_train,
-        x_test,
-        y_test,
-        num_epochs=100,
-        batch_size=64,
-):
+def neural_net_model(input_shape):
     """
     Implement convolutional neural net using tensorflow API
-    :param x_train: (ndarray) of shape
-        (number of parameters, number of examples) Training data
-    :param y_train: (ndarray) of shape
-        (1, number of examples) Training true values
-    :param x_test: (ndarray) of shape
-        (number of parameters, number of examples) Test true values
-    :param y_test: (ndarray) of shape (1, number of examples) Test data
-    :param num_epochs: (int)
-    :param batch_size: (int)
-    :return:
+    :param input_shape: (tuple) Shape of the input data
+        (widht, height, channels)
+    :return: (tf.model)
     """
-    input_layer = tf.keras.Input(shape=x_train.shape[1:])
+
+    input_layer = tf.keras.Input(shape=input_shape)
 
     z1 = tfl.Conv2D(8, 4, padding="same")(input_layer)
     a1 = tfl.ReLU()(z1)
@@ -42,8 +31,107 @@ def neural_net_model(
     conv_model.compile(
         optimizer="adam",
         loss="binary_crossentropy",
-        metrics=["binary_accuracy"],
+        metrics=[
+            "binary_accuracy",
+            tf.keras.metrics.FalseNegatives(),
+            tf.keras.metrics.FalsePositives(),
+        ],
     )
+
+    return conv_model
+
+
+def visualize(history, num_epochs, num_examples):
+    """
+    Visualize training process of the neural net
+    :param history: (tf.History) Result of tf.Model.fit()
+    :param num_epochs: (int)
+    :param num_examples: (int)
+    :return: (None)
+    """
+    fig, ax = plt.subplots()
+    ax.plot(history.history["loss"], label="Train Loss")
+    ax.plot(history.history["val_loss"], label="Test Loss")
+    ax.legend(loc="upper right")
+    plt.ylabel("Loss value")
+    plt.xlabel("Epochs")
+    plt.axis([0, num_epochs, 0, 1])
+    plt.show()
+
+    train_accuracy = np.array(history.history["binary_accuracy"]) * 100
+    test_accuracy = np.array(history.history["val_binary_accuracy"]) * 100
+    fig, ax = plt.subplots()
+    ax.plot(
+        train_accuracy,
+        label="Train Accuracy %",
+    )
+    ax.plot(
+        test_accuracy,
+        label="Test Accuracy %",
+    )
+    ax.legend(loc="lower right")
+    plt.ylabel("Accuracy value %")
+    plt.xlabel("Epochs")
+    plt.axis([0, num_epochs, 0, 100])
+    plt.show()
+
+    false_negatives_train = np.array(history.history["false_negatives"])
+    false_negatives_train = 100 * false_negatives_train / num_examples
+
+    false_negatives_test = np.array(history.history["val_false_negatives"])
+    false_negatives_test = 100 * false_negatives_test / num_examples
+    fig, ax = plt.subplots()
+    ax.plot(
+        false_negatives_train,
+        label="Train False Negatives %",
+    )
+    ax.plot(
+        false_negatives_test,
+        label="Test False Negatives %",
+    )
+    ax.legend(loc="upper right")
+    plt.ylabel("False Negatives %")
+    plt.xlabel("Epochs")
+    plt.axis([0, num_epochs, 0, 100])
+    plt.show()
+
+    false_positives_train = np.array(history.history["false_positives"])
+    false_positives_train = 100 * false_positives_train / num_examples
+
+    false_positives_test = np.array(history.history["val_false_positives"])
+    false_positives_test = 100 * false_positives_test / num_examples
+    fig, ax = plt.subplots()
+    ax.plot(
+        false_positives_train,
+        label="Train False Positives %",
+    )
+    ax.plot(
+        false_positives_test,
+        label="Test False Positives %",
+    )
+    ax.legend(loc="upper right")
+    plt.ylabel("False Positives %")
+    plt.xlabel("Epochs")
+    plt.axis([0, num_epochs, 0, 100])
+    plt.show()
+
+
+def run_model(num_epochs=100, batch_size=32):
+    """
+    Service method to run neural net
+    :param num_epochs: (int)
+    :param batch_size: (int)
+    :return: (None)
+    """
+    dataset = h5py.File(dogsml.utils.dataset.NATURAL_IMAGES_HDF5_CONV, "r")
+    x_train = dataset["x_dev"]
+    y_train = dataset["y_dev"]
+    x_test = dataset["x_test"]
+    y_test = dataset["y_test"]
+
+    conv_model = neural_net_model(x_train.shape[1:])
+
+    num_examples = x_train.shape[0]
 
     train_dataset = tf.data.Dataset.from_tensor_slices(
         (x_train, y_train)
@@ -52,33 +140,14 @@ def neural_net_model(
         (x_test, y_test)
     ).batch(batch_size)
 
-    conv_model.fit(
+    history = conv_model.fit(
         train_dataset,
         epochs=num_epochs,
-        validation_data=test_dataset
+        validation_data=test_dataset,
     )
 
-
-def run_model():
-    """
-    Service method to run neural net
-    :return: (None)
-    """
-    dataset = h5py.File(dogsml.utils.dataset.NATURAL_IMAGES_HDF5_CONV, "r")
-    x_train = dataset["x_train"]
-    y_train = dataset["y_train"]
-    x_test = dataset["x_test"]
-    y_test = dataset["y_test"]
-
-    neural_net_model(
-        x_train,
-        y_train,
-        x_test,
-        y_test,
-        num_epochs=500,
-        batch_size=32,
-    )
+    visualize(history, num_epochs, num_examples)
 
 
 if __name__ == '__main__':
-    run_model()
+    run_model(num_epochs=10, batch_size=350)
