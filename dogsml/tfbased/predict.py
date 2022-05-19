1a8dc9a81f758683f6514665c6df85f87df3796d
maxsitnikov <makc.kekc@gmail.com>
2022-05-20 01:43:04 +0300

    Create a way to run tf.model on the image url

--- dogsml/tfbased/predict.py
+++ dogsml/tfbased/predict.py
@@ -2,10 +2,25 @@ import numpy as np
 import os
 import tensorflow as tf
 
+import dogsml.settings
 import dogsml.utils
+import dogsml.tfbased.utils
+
+
+__all__ = (
+    "predict",
+    "predict_from_url",
+    "interpret_predicted_dog_values",
+)
 
 
 def predict(image_folder):
+    """
+    Run model on the image folder.
+    Return numpy array with probabilities (0 < p < 1)
+    :param image_folder: str
+    :return: ndarray
+    """
     image_data = []
     image_names = []
     for filename in os.listdir(image_folder):
@@ -15,22 +30,36 @@ def predict(image_folder):
             dogsml.utils.dataset.extract_image_data_from_path(image_path)
         )
     image_data = np.array(image_data)
-    print("image_data.shape", image_data.shape)
     conv_model = tf.keras.models.load_model(
         "{0}/conv_model_500".format(
-            dogsml.utils.dataset.COMPILED_MODELS_PATH
+            dogsml.settings.COMPILED_MODELS_PATH
         )
     )
-    predicted_values = conv_model.predict(image_data)
-    for index in range(len(image_names)):
-        print("{0} === {1}".format(
-            image_names[index],
-            predicted_values[index]),
-        )
     return conv_model.predict(image_data)
 
 
-if __name__ == '__main__':
-    predict("{0}/dog_breeds/images/Images/n02085620-Chihuahua".format(
-        dogsml.utils.dataset.DATA_ROOT)
+def predict_from_url(url):
+    """
+    Load TF model and run it using image url
+    :param url: (str)
+    :return: float
+    """
+    data = dogsml.tfbased.utils.read_tensor_from_image_url(url)
+    conv_model = tf.keras.models.load_model(
+        "{0}/conv_model_500".format(
+            dogsml.settings.COMPILED_MODELS_PATH
+        )
     )
+    predicted_values = conv_model.predict(data)
+    return predicted_values[0][0]
+
+
+def interpret_predicted_dog_values(value):
+    """
+    Convert probability to human like language
+    :param value: float
+    :return: str
+    """
+    if value >= 0.5:
+        return "It is a dog"
+    return "It is not a dog"
--- dogsml/tfbased/utils.py
+++ dogsml/tfbased/utils.py
@@ -1,3 +1,4 @@
+import requests
 import tensorflow as tf
 
 
@@ -42,3 +43,27 @@ def initialize_params(x_train, y_train, layer1_size):
     w2 = tf.Variable(initializer(shape=(output_layer_size, layer1_size)))
     b2 = tf.Variable(initializer(shape=(output_layer_size, 1)))
     return w1, b1, w2, b2
+
+
+def read_tensor_from_image_url(
+    url,
+    input_height=64,
+    input_width=64,
+    input_std=255,
+):
+    """
+    Download image from url and prepare a tf.Tensor
+    :param url: (str)
+    :param input_height: (int)
+    :param input_width: (height)
+    :param input_std: (int) - max value of the single channel
+    :return: (tf.Tensor: shape=(1, 64, 64, 3), dtype=float32)
+    """
+    image_reader = tf.image.decode_jpeg(
+        requests.get(url).content, channels=3, name="jpeg_reader")
+    float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(float_caster, 0)
+    resized = tf.image.resize(dims_expander, [input_height, input_width])
+    normalized = tf.divide(resized, [input_std])
+
+    return normalized
